8dbd38570045abf8be6cb41c519a14910a24b028
James Wolfe <cubswolfe@gmail.com>
2020-01-16 17:56:25 -0500

    Intake cycling system coded. Needs to talk to shooter.java to get timing right. Cycling timing untested. No communication for shooter.java to get supplided a ball.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,21 +1,107 @@
-package frc.robot.subsystems;
+package frc.robot;
 
-/** 
- * This is the code for the power cell intake. It initializes motor controllers and has methods
- * for the intake.
- * @author 
+import edu.wpi.first.wpilibj.XboxController;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
+/**
+ * The Hopper subsystem consists of 3 motors to move the power cells in stages. Positions one and
+ * two are on the bottom, position three is the transition to the upper level of the hopper, and
+ * positions four and five are on the top level.
+ * 
+ * @author dri, JW, AF
  */
+
 public class Intake {
     private static Intake instance;
 
-    private Intake() {
+    private DigitalInput botLeft = new DigitalInput(0);
+    private DigitalInput botRight = new DigitalInput(1);
 
-    }
+    // private DigitalInput midLeft = new DigitalInput(2);
+    // private DigitalInput midRight = new DigitalInput(3);
+
+    private DigitalInput topLeft = new DigitalInput(4);
+    private DigitalInput topRight = new DigitalInput(5);
+
+    private XboxController controller = new XboxController(0);
+
+    private WPI_TalonSRX intake = new WPI_TalonSRX(2);
+    private WPI_TalonSRX middle = new WPI_TalonSRX(0);
+    private WPI_TalonSRX outtake = new WPI_TalonSRX(21);
+
+    private int count = 0;
+    private int prev_count = 0;
+
+    private Intake() {}
 
     public static Intake getInstance() {
         if (instance == null) {
-            return new Intake();
+            instance = new Intake();
         }
         return instance;
+    }
+
+    public void initialize() {
+        middle.setInverted(true);
+        intake.setInverted(true);
+        outtake.setInverted(true);
+    }
+
+    //in millaseconds
+    int mid_cycle_time = 100000;
+    int chain_cycle_time = 100000; 
+
+    public void update() { //TODO: add logic back in
+        
+        if(prev_count < count) {  //add a ball
+            intake.set(-1); 
+            middle.set(-1);
+            outtake.set(-1);
+            Thread.sleep(mid_cycle_time/2);
+            middle.set(0);
+            Thread.sleep(chain_cycle_time-mid_cycle_time/2);
+            outtake.set(0);
+            intake.set(0);
+
+            prev_count = count;
+        } else if(prev_count > count) {  // want to remove a ball
+            for(int i = 0;i<=5-count;i++)
+            {
+                intake.set(-1); 
+                middle.set(-1);
+                outtake.set(-1);
+                Thread.sleep(mid_cycle_time/2);
+                middle.set(0);
+                Thread.sleep(chain_cycle_time-mid_cycle_time/2);
+                outtake.set(0);
+                intake.set(0);
+            }
+            //ping shooter ready
+            //or
+            //set shooter to delay -- chain_cycle_time * i calculation (extra forloop) 
+            count --;
+            prev_count = count;
+
+        }
+    }
+
+    public boolean readyToShoot() {
+        return top();
+    }
+
+    private boolean bottom() {
+        count++;
+        return botLeft.get() || botRight.get();
+    }
+
+    private boolean top() {
+        return topLeft.get() || topRight.get();
+    }
+
+    public void driveStraight(double targetDist) {
+
     }
 }
