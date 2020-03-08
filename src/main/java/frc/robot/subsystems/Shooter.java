222fc757f2ccd49f8babe0f81e37a172d0d7afbe
Dee <35465486+20iyerd@users.noreply.github.com>
2020-03-08 00:41:28 -0500

    removed SD pid gains, since they're hardcoded now

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -45,13 +45,6 @@ public class Shooter {
         secondary.follow(primary, RobotMap.Shooter.SECONDARY_IS_OPPOSITE);
         
         pid = primary.getPIDController();
-
-        // SmartDashboard.putNumber("kP", 0);
-        // SmartDashboard.putNumber("kI", 0);
-        // SmartDashboard.putNumber("kD", 0);
-        // SmartDashboard.putNumber("kFF", 0);
-        // SmartDashboard.putNumber("kIa", 0);
-        // SmartDashboard.putNumber("Setpoint", 0);
     }
 
     /**
@@ -69,19 +62,6 @@ public class Shooter {
      * Sets the PID gains and setpoint for the PID controller.
      */
     public void init() {
-        // kP = SmartDashboard.getNumber("kP", 0);
-        // kI = SmartDashboard.getNumber("kI", 0);
-        // kD = SmartDashboard.getNumber("kD", 0);
-        // kFF = SmartDashboard.getNumber("kFF", 0);
-        // kIa = SmartDashboard.getNumber("kIa", 0);
-        // setpoint = SmartDashboard.getNumber("Setpoint", RobotMap.Shooter.SHOOTER_DEFAULT_SPEED);
-
-        // pid.setP(kP/1000);
-        // pid.setI(kI/1000);
-        // pid.setD(kD/1000);
-        // pid.setFF(kFF/1000);
-        // pid.setIMaxAccum(kIa, 0);
-
         kP = 0.2/1000;
         kI = 0.00001/1000;
         kD = 0.03/1000;
